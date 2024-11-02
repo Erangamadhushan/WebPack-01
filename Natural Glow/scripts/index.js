c564ef849f1ef83326257fbd5580e7538f27bf1e
Erangamadhushan <erangamadhushann956@gmail.com>
2024-11-03 01:10:08 +0530

    add page content into the index.html file(like testiminols) with js and related resoureces and styles

--- Natural Glow/scripts/index.js
+++ Natural Glow/scripts/index.js
@@ -1,9 +1,12 @@
 import {products} from './products.js';
+import { testiminals } from './terminologies.js';
 
 let products_container = document.querySelector('.products_container');
+let terminologie = document.querySelector('.terminologies');
 
 window.addEventListener('DOMContentLoaded',() => {
     renderitems(products);
+    renderterminologies(testiminals);
 });
 
 function renderitems(products) {
@@ -23,4 +26,30 @@ function renderitems(products) {
     });
 
     products_container.innerHTML = products_content;
+}
+
+function renderterminologies(testiminals) {
+    terminologie.innerHTML = " ";
+    let terminologies_content = " ";
+    testiminals.forEach((item,index) => {
+        terminologies_content += `
+            <div class="col-lg-3 col-md-4 col-sm-6 border-line rounded-2  py-4 mb-3">
+                <div class="d-grid" style="display:grid;place-items:center;">
+                    <div>
+                        <p class="text-center">${item.desc}</p>
+                    </div>
+                    <div class="row row-cols-2">
+                        <div class="col-5">
+                            <img src="${item.user}" alt="services_images" style="width:100px;aspect-ratio:1;object-fit:cover;"/>
+                        </div>
+                        <div class="col-7">
+                            <h4 class="text-center main_heading py-2">${item.userName}</h4>
+                            <p class="text-center main_heading">${item.rating}/10</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+    });
+    terminologie.innerHTML = terminologies_content;
 }
--- Natural Glow/scripts/products.js
+++ Natural Glow/scripts/products.js
@@ -25,39 +25,39 @@ export const products = [
     },
     {
         title:"Item 005",
-        img:"./images/products/item001.jpg",
+        img:"./images/products/item005.jpg",
         desc:"This is a Item005 Description for customers",
         price:15.99
     },
     {
         title:"Item 006",
-        img:"./images/products/item002.jpg",
+        img:"./images/products/item006.jpg",
         desc:"This is a Item006 Description for customers",
         price:15.99
     },
     {
         title:"Item 007",
-        img:"./images/products/item003.jpg",
+        img:"./images/products/item007.jpg",
         desc:"This is a Item007 Description for customers",
         price:15.99
     },
     {
         title:"Item 008",
-        img:"./images/products/item004.jpg",
+        img:"./images/products/item008.jpg",
         desc:"This is a Item008 Description for customers",
         price:15.99
     },
     {
         title:"Item 009",
-        img:"./images/products/item001.jpg",
+        img:"./images/products/item009.jpg",
         desc:"This is a Item009 Description for customers",
         price:15.99
     },
     {
         title:"Item 010",
-        img:"./images/products/item002.jpg",
+        img:"./images/products/item010.jpg",
         desc:"This is a Item010 Description for customers",
         price:15.99
     },
     
-]
+]; 
