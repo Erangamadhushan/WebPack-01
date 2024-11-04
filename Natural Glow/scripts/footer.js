5e407b8b35f8bfb38871eace808e53c06ef44e70
Erangamadhushan <erangamadhushann956@gmail.com>
2024-11-04 15:47:12 +0530

    load footer content and add some addtional html files into the work folder.such as, About.html,complain.html,Service.html and footer .js as well as add the social media icons into the icons folder

---
+++ Natural Glow/scripts/footer.js
@@ -0,0 +1,38 @@
+export const content1 = [
+    {
+        title:'Natura Glow',
+        desc:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. ',
+        facebookImg:'./icons/socialMedia/facebook.png',
+        instagramImg:'./icons/socialMedia/instagram.png',
+        xImg:'./icons/socialMedia/x.png',
+        extraContent:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+    }
+];
+
+export const content2 = [
+    {
+        title:'Links',
+        link1:'products.html',
+        link2:'About.html',
+        link3:'Service.html',
+        link4:'complain.html'
+        
+    }
+];
+
+export const content3 = [
+    {
+        title:'Natura Glow',
+        img:''
+        
+    }
+];
+
+export const content4 = [
+    {
+        title:'Power by EM956 Community',
+        topic:'Subscribe Our New Later',
+        buttonValue:'Subscribe'
+        
+    }
+];
--- Natural Glow/scripts/index.js
+++ Natural Glow/scripts/index.js
@@ -1,5 +1,6 @@
 import {products} from './products.js';
 import { testiminals } from './terminologies.js';
+import {content1,content2,content3,content4} from './footer.js';
 
 let products_container = document.querySelector('.products_container');
 let terminologie = document.querySelector('.terminologies');
@@ -7,6 +8,7 @@ let terminologie = document.querySelector('.terminologies');
 window.addEventListener('DOMContentLoaded',() => {
     renderitems(products);
     renderterminologies(testiminals);
+    loadFooter();
 });
 
 function renderitems(products) {
@@ -52,4 +54,60 @@ function renderterminologies(testiminals) {
         `
     });
     terminologie.innerHTML = terminologies_content;
+}
+
+function loadFooter(){
+    let footer = document.querySelector('.footer');
+    footer.innerHTML = " ";
+    let footerContent = " ";
+    content1.forEach((item) => {
+        footerContent += `
+            <div class="col-md-3 col-sm-6">
+                <h1 class="text-white">${item.title}</h1>
+                <p class="text-white">${item.desc}</p>
+                <div class="bg-main p-3 w-50 d-flex justify-content-center">
+                    <a href="" target="_blank"><img src="${item.facebookImg}" alt="facebookPageIcon" width="50"/></a>
+                    <a href="" target="_blank"><img src="${item.instagramImg}" alt="instagramPageIcon" width="50"/></a>
+                    <a href="" target="_blank"><img src="${item.xImg}" alt="xAccountIcon" width="50"/></a>
+                </div>
+                <p class="text-white">${item.extraContent}</p>
+            </div>
+        `
+    });
+
+    content2.forEach((item) => {
+        footerContent += `
+            <div class="col-md-3 col-sm-6">
+                <h2 class="text-white">${item.title}</h2>
+                <p class="text-white"><a href="${item.link1}">Products</a></p>
+                <p><a href="${item.link2}">About Us</a></p>
+                <p><a href="${item.link3}">Services</a></p>
+                <p><a href="${item.link4}">Complain</a></p>
+            </div>
+        `
+    });
+
+    content3.forEach((item) => {
+        footerContent += `
+            <div class="col-md-3 col-sm-6">
+                <h1 class="text-center text-white">${item.title}</h1>
+                <img src="${item.img}" alt="Natura Glow logo" style="max-width:100%;"/>
+            </div>
+        `
+    });
+
+    content4.forEach((item) => {
+        footerContent += `
+            <div class="col-md-3 col-sm-6">
+                <div class="d-grid' style="display:grid;place-items:center;">
+                    <h1 class="text-center text-white">${item.title}</h1>
+                    <p class="text-white">${item.topic}</p>
+                    <button type="button" class="line_btn">${item.buttonValue}</button>
+                </div>
+            </div>
+        `
+    });
+
+    console.log(footerContent);
+    footer.innerHTML = footerContent;
 }
