beaaa18a30a213ccf4c6cf2eaadbb202b30820b7
Erangamadhushan <erangamadhushann956@gmail.com>
2024-11-15 21:59:27 +0530

    add basic structure details into footer.js file

--- Construction Company/scripts/footer.js
+++ Construction Company/scripts/footer.js
@@ -2,23 +2,41 @@ export const  footer1Content1 = {
     topic:"About Us",
     desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores hic eius distinctio totam culpa! Officiis expedita, debitis veniam obcaecati saepe perspiciatis deserunt nemo soluta nisi eum dolor in omnis excepturi."
 }
+
 export const  footer1Content2 = {
     img:"./images/default/building.jpg",
     topic:"Lorem Ipsum dolor",
     desc:" Dolores hic eius distinctio totam culpa! Officiis expedita, debitis veniam obcaecati saepe perspiciatis deserunt nemo soluta nisi eum dolor in omnis excepturi."
 }
+
 export const  footer1Content3 = {
-    topic:"Get Info"
+    topic:"Get Info",
+    img1:"./icons/socialMedia/facebook.png",
+    img2:"./icons/socialMedia/instagram.png",
+    img3:"./icons/socialMedia/x.png",
+    playStoreImg:"./icons/playStore.png",
+    appleStoreImg:"./icons/appleStore.png"
 }
+
 export const  footer2Content1 = {
-    
+    topic:"About Us",
+    items1:"Customer Reviews",
+    items2:"Complains",
+    items3:"Special Offers"
 }
+
 export const  footer2Content2 = {
-
+    topic:"Services",
+    items1:"House Building Supports",
+    items2:"Appartment Building"
 }
+
 export const  footer2Content3 = {
-
+    content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit."
 }
+
 export const  footer2Content4 = {
-
+    content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit."
 }
+
+
