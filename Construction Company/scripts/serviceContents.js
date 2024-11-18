27e0e99f25d3e7159b763278f330104f5988ccf0
Erangamadhushan <erangamadhushann956@gmail.com>
2024-11-18 18:13:06 +0530

    create category content container

--- Construction Company/scripts/serviceContents.js
+++ Construction Company/scripts/serviceContents.js
@@ -3,29 +3,39 @@ import {items} from './itemContent.js';
 
 
 window.addEventListener("DOMContentLoaded", () => {
-    renderCategoryNames(AllcategoryContents);
+    //renderCategoryNames(AllcategoryContents);
     renderCategoryDetails(AllcategoryContents);
 });
 
-function renderCategoryNames(categoryContents) {
-    // categoryContents.forEach((categoryContent) => {
-    //     console.log(categoryContent);
-    // })
-
-    console.log(categoryContents[0]);
-}
 
 function renderCategoryDetails(categories) {
+    let outputCategoryContents = document.querySelector('.categoryContents');
+    let categoryContentItem = " "; 
     categories.forEach((category) => {
+        categoryContentItem += `<h1>${category.topic}</h1>`;
         category.forEach((categoryItems) => {
-            //console.log(categoryItems);
+            categoryContentItem += `
+                <div class="col-md-8">
+                    <div class="row row-cols-2">
+                        <div class="col-4">
+
+                        </div>
+                        <div class="col-8">
+
+                        </div>
+                    </div>
+                </div>
+            `
         })
-    })
+    });
+    outputCategoryContents.innerHTML = categoryContentItem;
 }
 
 //load categoryMenu content
 
 let CategoryMenu = document.querySelector('.categoryMenu');
+
+
 CategoryMenu.innerHTML = " ";
 let Categories = [];
 let categorycontent = " ";
@@ -35,14 +45,12 @@ items.forEach((apartment) => {
     }
 })
 Categories.forEach((ele) => {
-    console.log(ele);
     categorycontent += `
         <div class="col-md-2 col-sm-5 mt-3" style="display:grid;place-items:center;" data-aos="fade-right" data-aos-duration="400" data-aos-easing="linear" data-aos-anchor-placement="bottom-bottom">
             <button type="button" class="btn-outline-main">  ${ele}</button>
         </div>  
     `;
     
-
 
 });
 CategoryMenu.innerHTML = categorycontent;
