9f9a5888d3ba3ff285f1d34476ab907f08d58ebd
Erangamadhushan <erangamadhushann956@gmail.com>
2024-11-25 00:13:25 +0530

    create index.html file navbar ,welcome text container, mobile response navbar and about us contents with items images and descriptions load by main.js,mainContent.js,itemContents.js and jqueryAction.js(for create mobile response navbar)

---
+++ Aqme Company/src/scripts/sources/itemContents.js
@@ -0,0 +1,103 @@
+export const itemQuatar = [
+    {
+        img:"./images/watches/watchtype1.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:120.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+
+    },
+    {
+        img:"./images/watches/watchtype4.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:150.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+    },
+    {
+        img:"./images/watches/watchtype7.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:220.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+    },
+    {
+        img:"./images/watches/watchtype10.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:90.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+    }
+];
+
+export const allItems = [
+    {
+        img:"./images/watches/watchtype1.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:135.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+    },
+    {
+        img:"./images/watches/watchtype2.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:125.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+    },
+    {
+        img:"./images/watches/watchtype3.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:120.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+    },
+    {
+        img:"./images/watches/watchtype4.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:150.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+    },
+    {
+        img:"./images/watches/watchtype5.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:120.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+    },
+    {
+        img:"./images/watches/watchtype6.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:199.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+    },
+    {
+        img:"./images/watches/watchtype7.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:179.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+    },
+    {
+        img:"./images/watches/watchtype8.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:110.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+    },
+    {
+        img:"./images/watches/watchtype9.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:140.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+    },
+    {
+        img:"./images/watches/watchtype10.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:180.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+    },
+    {
+        img:"./images/watches/watchtype11.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:199.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+    },
+    {
+        img:"./images/watches/watchtype12.png",
+        brandName:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price:210.00,
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, assumenda quod, voluptatem ipsa eum eaque ad harum accusantium atque veniam facere voluptatum sed!"
+    },
+    
+]
---
+++ Aqme Company/src/scripts/sources/mainContent.js
@@ -0,0 +1,19 @@
+export const main = [
+    {
+        src:"./images/main.jpg",
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        subtext:"Lorem equal putient ouvioru su lorime",
+        topic:"Casio Edifice EFR-526L-JAVUEF",
+        price:"199.00",
+        desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quo doloribus qui id error voluptatem quibusdam enim ipsam ea ut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quo doloribus qui id error voluptatem quibusdam enim ipsam ea ut.",
+
+    }
+];
+export const aboutUs = {
+    topic:"About Us",
+    desc:"At <span class='text-red-500 font-bold'>Aqme Company PVT Ltd</span>, we believe a watch is more than a timepiece—it's a statement of style, precision, and individuality. Founded with a passion for craftsmanship and innovation, we curate a diverse selection of wristwatches to cater to every taste, whether you seek timeless elegance, cutting-edge technology, or everyday functionality. Our collection showcases iconic brands and exclusive designs, blending heritage and modernity to offer you the perfect accessory for every occasion. From classic analog designs to feature-packed smartwatches, each piece reflects our commitment to quality and excellence. Driven by a dedication to customer satisfaction, we provide a seamless shopping experience with expert guidance and unparalleled service. Whether you’re a seasoned collector or searching for your first wristwatch, [Your Company Name] is here to help you find a watch that resonates with your personality and lifestyle."
+
+}
+
